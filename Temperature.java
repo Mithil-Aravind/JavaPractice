3349276c6e7fa78277aa7eff904f8f407053e4ae
Mithil-Aravind <jokestermithil@gmail.com>
2026-06-22 11:52:30 -0700

    Temperature Converter
    Created a temperature converter file in java.

---
+++ Temperature.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("1. C->F");
+        System.out.println("2. F->C");
+
+        double type = userInput.nextDouble();
+        System.out.println("How many degrees is it? ");
+        double degrees = userInput.nextDouble();
+
+        if (type == 1) {
+            double converted = degrees * (9.0/5) + 32;
+            System.out.println(degrees + " celsius is " + converted + " fahrenheit");
+        } else if (type == 2) {
+            double converted = (degrees -32) * 5.0/9;
+            System.out.println(degrees + " fahrenheit is " + converted + " celsius");
+        } else {
+            System.out.println("That is not an option.");
+        }
+    }
+}
